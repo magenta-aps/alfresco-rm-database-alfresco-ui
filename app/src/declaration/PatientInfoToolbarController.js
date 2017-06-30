816efb8083e3a865dd93b3f6c30966bc938b79ad
Anders Jepsen <hi@andersjepsen.com>
2017-06-30 15:59:03 +0200

    added tests for searching, creating, and editing cases

--- app/src/declaration/PatientInfoToolbarController.js
+++ app/src/declaration/PatientInfoToolbarController.js
@@ -2,7 +2,7 @@ angular
     .module('openDeskApp.declaration')
     .controller('PatientInfoToolbarController', PatientInfoToolbarController);
 
-function PatientInfoToolbarController($scope, $state, $stateParams, declarationService) {
+function PatientInfoToolbarController($scope, $mdDialog, $state, $stateParams, declarationService) {
 
     $scope.declarationService = declarationService;
     $scope.editMode = false;
@@ -27,6 +27,17 @@ function PatientInfoToolbarController($scope, $state, $stateParams, declarationS
         var newCase = declarationService.getNewCaseInfo();
         declarationService.updateCase($stateParams.caseid,newCase).then(function(response) {
             console.log(response);
+        });
+    }
+
+    $scope.lockCase = function() {
+        $mdDialog.show({
+            templateUrl: 'app/src/declaration/view/lock-dialog.html',
+            parent: angular.element(document.body),
+            targetEvent: event,
+            scope: $scope, // use parent scope in template
+            preserveScope: true, // do not forget this if use parent scope
+            clickOutsideToClose: true
         });
     }
 
--- app/tests/e2e/declarations/declarations.test.js
+++ app/tests/e2e/declarations/declarations.test.js
@@ -1,3 +1,41 @@
+var createDeclaration = require('./createDeclaration.po.js');
+var editDeclaration = require('./editDeclaration.po.js');
+var searchDeclaration = require('./searchDeclaration.po.js');
+
 describe('declarations', function() {
-    
+    it('should go to create new declaration page', function() {
+        createDeclaration.goToCreateDeclarationPage();
+        expect(browser.getCurrentUrl()).toContain('/erklaeringer/opret');
+    });
+
+    it('should fill in basic info', function() {
+        createDeclaration.fillInputFields();
+        createDeclaration.createDeclaration();
+        expect(browser.getCurrentUrl()).toContain('/patientdata');
+        createDeclaration.waitForSolr();
+        expect(createDeclaration.getDeclarationTitle()).toContain('Jens Rasmussen');
+    });
+
+    it('should not be able to edit the fields', function() {
+        expect(editDeclaration.findMainCharge().getAttribute('disabled')).toBe('true');
+    })
+
+    it('should edit the declaration', function() {
+        editDeclaration.editDeclarationPage();
+        expect(browser.getCurrentUrl()).toContain('/patientdata/rediger');
+        editDeclaration.fillInputFields();
+        editDeclaration.finishEditing();
+        expect(editDeclaration.findMainCharge().getAttribute('value')).toBe('Disrupter');
+    })
+
+    it('should go back to search', function() {
+        searchDeclaration.goBack();
+        expect(browser.getCurrentUrl()).toContain('/erklaeringer');
+    });
+
+    it('should search for a case', function() {
+        searchDeclaration.fillInputFields();
+        searchDeclaration.search();
+        expect(browser.getCurrentUrl()).toContain('/erklaeringer/13/patientdata');
+    });
 });
