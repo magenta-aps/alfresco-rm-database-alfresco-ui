eb35540ec800de71940543c38e36212c6c26ab3b
Steffen Jørgensen <sj@magenta-aps.dk>
2019-01-29 11:56:25 +0100

    Fixed navigation from buttons to links + added padding to bottom to show logo + fixed icon position

--- app/src/system_settings/systemSettingsController.js
+++ app/src/system_settings/systemSettingsController.js
@@ -4,18 +4,17 @@ angular
     .module('openDeskApp.systemsettings')
     .controller('SystemSettingsController', SystemSettingsCtrl);
 
-function SystemSettingsCtrl($scope, $state, $translate, authService, HeaderService) {
+function SystemSettingsCtrl($scope, $translate, authService, HeaderService) {
     var vm = this;
 
     $scope.templateSites = [];
 
-    vm.viewState = viewState;
+    vm.changePageHeading = changePageHeading;
 
     HeaderService.resetActions();
     HeaderService.setTitle($translate.instant('ADMIN.ADMINISTRATION'));
 
-    function viewState(newState) {
-        $state.go('administration.' + newState);
+    function changePageHeading(newState) {
         HeaderService.setTitle($translate.instant('ADMIN.ADMINISTRATION') + ' - ' + $translate.instant('ADMIN.' + newState.toUpperCase()));
     }
 
