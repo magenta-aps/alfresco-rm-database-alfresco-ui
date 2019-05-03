1edfad74e82274e43c2c497aca4695ea9e08b1d1
Flemming Heide Pedersen <fhp@magenta-aps.dk>
2019-05-03 10:54:39 +0200

    debugging

--- app/src/lool/loolController.js
+++ app/src/lool/loolController.js
@@ -31,38 +31,30 @@ function LoolController($stateParams, loolService) {
 
             loolService.getState(vm.nodeRef).then(function (response){
 
-            console.log("response");
-            console.log(response);
+                if (response) {
+                    alert("dokumentet er låst og redigeres af en anden bruger");
+                }
+                else {
+                    loolService.markDocumentAsEditing(vm.nodeRef)
 
+                    loolService.getWopiUrl(vm.nodeRef).then(function (response) {
+                        var shortRef = vm.nodeRef.substring(vm.nodeRef.lastIndexOf('/') + 1);
+                        var wopi_src_url = response.wopi_src_url;
+                        var wopiFileURL = serviceUrl + "/wopi/files/" + shortRef;
+                        var frameSrcURL = wopi_src_url + "WOPISrc=" + encodeURIComponent(wopiFileURL);
+                        var access_token = encodeURIComponent(response.access_token);
+                        //Use JQuery to submit the form and 'target' the iFrame
+                        $(function () {
+                            var form = '<form id="loleafletform" name="loleafletform" target="loleafletframe" action="' + frameSrcURL + '" method="post">' +
+                                '<input name="access_token" value="' + encodeURIComponent(access_token) + '" type="hidden"/></form>';
 
-//                console.log("doing if");
-//                                loolService.markDocumentAsEditing(vm.nodeRef)
-//                            }
-//                            else {
-//                            console.log("doing else");
-//                                alert("dokumentet er låst og redigeres af en anden bruger");
-//                            }
-
-
-
+                            $('#libreoffice-online').append(form);
+                            $('#loleafletform').submit();
+                        });
+                    });
+                }
             })
 
-
-        loolService.getWopiUrl(vm.nodeRef).then(function (response) {
-            var shortRef = vm.nodeRef.substring(vm.nodeRef.lastIndexOf('/') + 1);
-            var wopi_src_url = response.wopi_src_url;
-            var wopiFileURL = serviceUrl + "/wopi/files/" + shortRef;
-            var frameSrcURL = wopi_src_url + "WOPISrc=" + encodeURIComponent(wopiFileURL);
-            var access_token = encodeURIComponent(response.access_token);
-            //Use JQuery to submit the form and 'target' the iFrame
-            $(function () {
-                var form = '<form id="loleafletform" name="loleafletform" target="loleafletframe" action="' + frameSrcURL + '" method="post">' +
-                    '<input name="access_token" value="' + encodeURIComponent(access_token) + '" type="hidden"/></form>';
-
-                $('#libreoffice-online').append(form);
-                $('#loleafletform').submit();
-            });
-        });
     }
 
      vm.goBack = function goBack() {
