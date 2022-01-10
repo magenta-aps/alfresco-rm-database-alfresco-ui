84b70800bb7ed3c6190481db999893edd74b7ca4
fhp <fhp@magenta-aps.dk>
2022-01-10 10:17:41 +0100

    fixed merge conflicts

--- app/src/declaration/declaration.controller.js
+++ app/src/declaration/declaration.controller.js
@@ -4,10 +4,7 @@ angular
   .module('openDeskApp.declaration')
   .controller('DeclarationController', DeclarationController);
 
-
-function DeclarationController($scope, propertyService, $state, $stateParams) {
-  $scope.case = {};
-  $scope.propertyValues = propertyService.getAllPropertyValues();
+function DeclarationController($scope, propertyService, $rootScope, HeaderService, $mdDialog, $state, $stateParams) {
 
   function gotoDocuments() {
     $state.go('declaration.show.documents', {
@@ -21,64 +18,55 @@ function DeclarationController($scope, propertyService, $state, $stateParams) {
 
   $scope.gotoDocuments = gotoDocuments;
 
-  function DeclarationController($scope, propertyService, $rootScope, HeaderService, $mdDialog, $state, $stateParams) {
-
-    var vm = this;
-
-    $scope.case = {};
-    $scope.propertyValues = propertyService.getAllPropertyValues();
-
-    function clearList() {
-      $rootScope.duf = null;
-    }
-
-    $scope.clearList = clearList();
-
-    $scope.authorityMailDialogMulti = authorityMailDialog;
-
-    function authorityMailDialog() {
-      $scope.selectedContent = $rootScope.duf;
-
-      $mdDialog.show({
-        templateUrl: 'app/src/authorityMail/authorityMail.view.html',
-        controller: 'AuthorityMailController as vm',
-        scope: $scope, // use parent scope in template
-        preserveScope: true, // do not forget this if use parent scope
-        clickOutsideToClose: false
-      }).then(function (response) {
-        $scope.clearList();
-        $state.go('declaration.show.documents', {
-          caseid: $stateParams.caseid,
-          breadcrumbPath: $stateParams.breadcrumbPath,
-          tmpNodeRef: $stateParams.breadcrumbPath[0].nodeUuid,
-          emailPayload: undefined,
-          selectedFiles: undefined
-        });
-      }).catch(function (response) {
-        $scope.clearList();
-        $state.go('declaration.show.documents', {
-          caseid: $stateParams.caseid,
-          breadcrumbPath: $stateParams.breadcrumbPath,
-          tmpNodeRef: $stateParams.breadcrumbPath[0].nodeUuid,
-          emailPayload: undefined,
-          selectedFiles: undefined
-        });
-      });
-    }
 
 
-    $scope.clearList = clearList;
+  var vm = this;
 
-    $scope.showMailAttachments = false;
+  $scope.case = {};
+  $scope.propertyValues = propertyService.getAllPropertyValues();
 
-    $rootScope.$watch('duf', function (newVal) {
-      if (newVal) {
-        $scope.duf = newVal;
-        $scope.showMailAttachments = true;
-      } else {
-        $scope.duf = undefined;
-        $scope.showMailAttachments = false;
-      }
+  function clearList() {
+    $rootScope.duf = null;
+  }
+   $scope.clearList = clearList();
+
+  $scope.authorityMailDialogMulti = authorityMailDialog;
+
+  function authorityMailDialog() {
+    $scope.selectedContent = $rootScope.duf;
+
+    $mdDialog.show({
+      templateUrl: 'app/src/authorityMail/authorityMail.view.html',
+      controller: 'AuthorityMailController as vm',
+      scope: $scope, // use parent scope in template
+      preserveScope: true, // do not forget this if use parent scope
+      clickOutsideToClose: false
+    }).then(function (response) {
+      $scope.clearList();
+      $state.go('declaration.show.documents', { caseid: $stateParams.caseid, breadcrumbPath: $stateParams.breadcrumbPath, tmpNodeRef : $stateParams.breadcrumbPath[0].nodeUuid, emailPayload : undefined, selectedFiles :  undefined});
+    }).catch(function (response) {
+      $scope.clearList();
+      $state.go('declaration.show.documents', { caseid: $stateParams.caseid, breadcrumbPath: $stateParams.breadcrumbPath, tmpNodeRef : $stateParams.breadcrumbPath[0].nodeUuid, emailPayload : undefined, selectedFiles :  undefined});
     });
   }
+
+
+
+  $scope.clearList = clearList;
+
+  $scope.showMailAttachments = false;
+
+  $rootScope.$watch('duf', function (newVal) {
+    if (newVal) {
+      $scope.duf = newVal;
+      $scope.showMailAttachments = true;
+    }
+    else {
+      $scope.duf = undefined;
+      $scope.showMailAttachments = false;
+    }
+  });
+
+
 }
+
